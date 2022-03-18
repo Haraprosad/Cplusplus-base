459bad33ffec0e093b95e2c70979ebc9fb8a8279
Haraprosad <haraprosadbiswas123@gmail.com>
2022-03-18 07:10:54 +0600

    Check prime number or not

---
+++ check_prime_number.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter your number:";
+    cin>>n;
+
+    bool flag = 0;
+    for(int i=2;i<=sqrt(n);i++){
+        if(n%i==0){
+            cout<<"Your Entered number is Not Prime-Number"<<endl;
+            flag = 1;
+            break;
+        }
+    }
+
+    if(flag==0){
+        cout<< "Your entered number is Prime";
+    }
+}
